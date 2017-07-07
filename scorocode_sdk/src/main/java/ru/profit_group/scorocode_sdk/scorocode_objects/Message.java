521506da345210286b4646b28768770140340159
peterstaranchuk <petr.staranchuk@gmail.com>
2017-07-07 13:27:03 +0300

    add rxjava support for Message class

--- scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/Message.java
+++ scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/Message.java
@@ -7,6 +7,8 @@ import ru.profit_group.scorocode_sdk.Callbacks.CallbackSendSms;
 import ru.profit_group.scorocode_sdk.Requests.messages.MessagePush;
 import ru.profit_group.scorocode_sdk.Requests.messages.MessageSms;
 import ru.profit_group.scorocode_sdk.ScorocodeSdk;
+import rx.Observable;
+import rx.Subscriber;
 
 /**
  * Created by Peter Staranchuk on 10/7/16
@@ -28,16 +30,92 @@ public class Message implements Serializable {
         ScorocodeSdk.sendPush(query.getCollectionName(), query, messagePush, isDebugMode, callback);
     }
 
+    public Observable<Void> sendPush(final MessagePush messagePush, final Query query) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override
+            public void call(final Subscriber<? super Void> subscriber) {
+                sendPush(messagePush, query, new CallbackSendPush() {
+                    @Override
+                    public void onPushSent() {
+                        subscriber.onCompleted();
+                    }
+
+                    @Override
+                    public void onPushSendFailed(String errorCode, String errorMessage) {
+                        subscriber.onError(new Throwable(errorCode + " " + errorMessage));
+                    }
+                });
+            }
+        });
+    }
+
     public void sendPush(MessagePush messagePush, CallbackSendPush callback) throws IllegalStateException {
         ScorocodeSdk.sendPush("users", null, messagePush, isDebugMode, callback);
+    }
+
+    public Observable<Void> sendPush(final MessagePush messagePush) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override
+            public void call(final Subscriber<? super Void> subscriber) {
+                sendPush(messagePush, null, new CallbackSendPush() {
+                    @Override
+                    public void onPushSent() {
+                        subscriber.onCompleted();
+                    }
+
+                    @Override
+                    public void onPushSendFailed(String errorCode, String errorMessage) {
+                        subscriber.onError(new Throwable(errorCode + " " + errorMessage));
+                    }
+                });
+            }
+        });
     }
 
     public void sendSms(MessageSms messageSms, Query query, CallbackSendSms callback) throws IllegalStateException {
         ScorocodeSdk.sendSms(query.getCollectionName(), query, messageSms, isDebugMode, callback);
     }
 
+    public Observable<Void> sendSms(final MessageSms messageSms, final Query query) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override
+            public void call(final Subscriber<? super Void> subscriber) {
+                sendSms(messageSms, query, new CallbackSendSms() {
+                    @Override
+                    public void onSmsSent() {
+                        subscriber.onCompleted();
+                    }
+
+                    @Override
+                    public void onSmsSendFailed(String errorCode, String errorMessage) {
+                        subscriber.onError(new Throwable(errorCode + " " + errorMessage));
+                    }
+                });
+            }
+        });
+    }
+
     public void sendSms(MessageSms messageSms, CallbackSendSms callback) throws IllegalStateException {
         ScorocodeSdk.sendSms("users", null, messageSms, isDebugMode, callback);
+    }
+
+    public Observable<Void> sendSms(final MessageSms messageSms) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override
+            public void call(final Subscriber<? super Void> subscriber) {
+                sendSms(messageSms, null, new CallbackSendSms() {
+                    @Override
+                    public void onSmsSent() {
+                        subscriber.onCompleted();
+                    }
+
+                    @Override
+                    public void onSmsSendFailed(String errorCode, String errorMessage) {
+                        subscriber.onError(new Throwable(errorCode + " " + errorMessage));
+                    }
+                });
+            }
+        });
     }
 
 }
