97deef867be22ec87e294c1dd0c5d0889d6210d9
peterstaranchuk <petr.staranchuk@gmail.com>
2017-07-07 13:26:02 +0300

    add rxjava support for appInfo

--- scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/ApplicationInfo.java
+++ scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/ApplicationInfo.java
@@ -2,6 +2,8 @@ package ru.profit_group.scorocode_sdk.scorocode_objects;
 
 import ru.profit_group.scorocode_sdk.Callbacks.CallbackGetApplicationInfo;
 import ru.profit_group.scorocode_sdk.ScorocodeSdk;
+import rx.Observable;
+import rx.Subscriber;
 
 /**
  * Created by Peter Staranchuk on 12/9/16
@@ -10,5 +12,25 @@ import ru.profit_group.scorocode_sdk.ScorocodeSdk;
 public class ApplicationInfo {
     public void getApplicationInfo(CallbackGetApplicationInfo callback) {
         ScorocodeSdk.getApplicationInfo(callback);
+    }
+
+    public Observable<ScorocodeApplicationInfo> getApplicationInfo() {
+        return Observable.create(new Observable.OnSubscribe<ScorocodeApplicationInfo>() {
+            @Override
+            public void call(final Subscriber<? super ScorocodeApplicationInfo> subscriber) {
+                getApplicationInfo(new CallbackGetApplicationInfo() {
+                    @Override
+                    public void onRequestSucceed(ScorocodeApplicationInfo appInfo) {
+                        subscriber.onNext(appInfo);
+                        subscriber.onCompleted();
+                    }
+
+                    @Override
+                    public void onRequestFailed(String errorCode, String errorMessage) {
+                        subscriber.onError(new Throwable(errorCode + " " + errorMessage));
+                    }
+                });
+            }
+        });
     }
 }
